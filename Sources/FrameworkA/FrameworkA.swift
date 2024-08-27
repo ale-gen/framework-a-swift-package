ab4118a4ad2674e00cc2cc24bf0d4501d5e724d4
Aleksandra Generowicz <o.generowicz@gmail.com>
2024-08-27 09:44:34 +0200

    Add files

---
+++ Sources/FrameworkA/FrameworkA.swift
@@ -0,0 +1,14 @@
+//
+//  FrameworkA.swift
+//  FrameworkA
+//
+//  Created by Aleksandra Generowicz on 26/08/2024.
+//
+
+import Foundation
+
+public enum FrameworkA {
+    public static func exampleFunction() -> String {
+        return "Example function from FrameworkA"
+    }
+}
